f6f6d5530ff7d3a0aa9ab8b3601a00d1b87a0478
lucashdoa <lucashdoa@gmail.com>
2023-07-05 01:08:03 -0300

    Test Case 1 Register User

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,29 @@
 import HomePage from '../pageobjects/HomePage';
+import LoginPage from '../pageobjects/LoginPage';
 
 describe('template spec', () => {
-  it('passes', () => {
+  it('TC1 - Register User', () => {
     const homePage = new HomePage();
+    const loginPage = new LoginPage();
     cy.visit('/');
-    homePage.clickTestCasesButton();
-    homePage.navigateToPage('products');
+    homePage.navigateToPage('login');
+    loginPage.enterSignupName('lucas');
+    loginPage.enterSignupEmail('lucas@test.com');
+    loginPage.clickSignupButton();
+    loginPage.selectGender('male');
+    loginPage.selectBirthDay(22);
+    loginPage.selectBirthMonth(10);
+    loginPage.selectBirthYear(94);
+    loginPage.enterPassword('p4ssw0rd');
+    loginPage.enterFirstName('Lucas');
+    loginPage.enterLastName('Andrade');
+    loginPage.enterAddress('Rua Tupis');
+    loginPage.enterState('Minas Gerais');
+    loginPage.enterCity('Uberlandia');
+    loginPage.enterZipcode('38408000');
+    loginPage.enterMobileNumber('32340000');
+    loginPage.clickCreateAccount();
+    loginPage.clickContinue();
+    loginPage.clickDeleteAccount();
   });
 });
--- cypress/pageobjects/BasePage.js
+++ cypress/pageobjects/BasePage.js
@@ -1,13 +1,27 @@
 class BasePage {
+  // Elements
   get productsNavLink() {
     return cy.get("a[href*='products']");
   }
+  get loginNavLink() {
+    return cy.get("a[href*='login']");
+  }
+  get deleteAccountLink() {
+    return cy.get("a[href*='delete account']");
+  }
 
+  // Methods
   navigateToPage(page) {
     switch (page) {
       case 'products':
         this.productsNavLink.click();
+      case 'login':
+        this.loginNavLink.click();
     }
+  }
+
+  clickDeleteAccount() {
+    this.deleteAccountLink.click();
   }
 }
 
